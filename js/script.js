fc259da6e89b765f63f3eda65c75537e467187a9
Franco Ferrer <franco.ferrer@gmail.com>
2022-04-18 23:53:06 -0300

    Incompleto2

--- js/script.js
+++ js/script.js
@@ -5,44 +5,65 @@ let stage = new Stage(0, "");
 init();
 
 function startStage(namePlayer) {
+  //Comienza partida
   //Inicia Partida de un jugador
+  console.log("startStage");
+  console.log(ranking.players);
   let player = ranking.players.find((player) => player.name === namePlayer);
+  console.log(player.name);
+  console.log("POST player");
   stage.attempts = 10;
   stage.playerName = player.name;
-  initBoard();
+  console.log(stage);
+  showHidePlayStopBtns();
+  //initBoard();
 }
 
 function initBoard() {
-  showPlayStopBtns();
+  //Inicializa tablero
 }
 
-function showPlayStopBtns(){
+function showHidePlayStopBtns() {
+  //Muestra-oculta botones de "Jugar"
+  console.log("Entra: " + stage.playerName);
+  
   const playBtns = document.getElementsByClassName("play");
-  console.log(playBtns.length);
-  for (let btn of playBtns) {
-    console.log(btn.classList.contains("player"+stage.playerName));
-    if (!btn.classList.contains("player"+stage.playerName)) {
-      btn.classList.add("d-none");
-      console.log(btn);
-    } else {
+  if (stage.playerName === "") {
+    for (let btn of playBtns) {
       btn.classList.remove("d-none");
-      btn.value = "Detener";
-      btn.addEventListener("click", stopStage());
+      btn.value = "Jugar";
+      btn.addEventListener("click", startStage);
+    }
+  } else { console.log("---ELSE---");
+    for (let btn of playBtns) {
+      if (!btn.classList.contains("player" + stage.playerName)) {
+        console.log("Invisible");
+        // btn.classList.add("d-none");
+        // btn.value = "-";
+      } else {
+        console.log("Detener");
+        // btn.classList.remove("d-none");
+        // btn.value = "Detener";
+        // //btn.addEventListener("click", stopStage);
       }
+    }
   }
+  console.log("Sale");
 }
 
-function stopStage(){
-  stage.attempts = 0;
-  stage.playerName = "";
-  saveRanking();
-  showRankingDOM();
+function stopStage() {
+  console.log("Detenido Juego");
+  // stage.attempts = 0;
+  // stage.playerName = "";
+  // saveRanking();
+  // showRankingDOM();
 }
 
 function init() {
   //Carga datos y funcionalidad de APP
   loadSave();
   addEvent();
+
 
   // TEST
   // let p1 = new Jugador("JuanP1");
@@ -100,19 +121,21 @@ function showRankingDOM() {
         "`)'>";
       ULPlayers.appendChild(liJugador);
     });
-    //showPlayStopBtns();
+    showHidePlayStopBtns();
   }
 }
 
 function addPlayer() {
   //Ingresa en sistema jugador a listado
   const inputPlayerName = document.getElementById("namePlayerText");
-  console.log(inputPlayerName.value);
-//  console.log(ranking.players.some(player => player.name === namePlayerText));
+  console.log("Input: " + inputPlayerName.value);
+  //  console.log(ranking.players.some(player => player.name === namePlayerText));
   if (inputPlayerName.value.length === 0) {
     showMsg("Favor ingrese un nombre.");
-  //} else if(ranking.players.some((player) => player.namePlayer === namePlayerText)) {
-  } else if(ranking.players.some(player => player.name === inputPlayerName)) {
+    //} else if(ranking.players.some((player) => player.namePlayer === namePlayerText)) {
+  } else if (
+    ranking.players.some((player) => player.name === inputPlayerName.value)
+  ) {
     showMsg("Nombre ya ingresado.");
   } else {
     const jugador = new Player(inputPlayerName.value);
@@ -124,7 +147,8 @@ function addPlayer() {
   }
 }
 
-function showMsg(textToShow){ //Muestra resultado en pantalla pantalla
+function showMsg(textToShow) {
+  //Muestra resultado en pantalla pantalla
   console.log(textToShow);
   const responseLbl = document.getElementById("responseAddPlayerBtn");
   responseLbl.innerText = textToShow;
@@ -165,8 +189,3 @@ function restarMarcador(jugador) {
   );
 }
 */
-function test(text) {
-  console.log(text);
-//console.log(ranking.players.some(player => player.namePlayer === text));
-  console.log(ranking.players.some(player => player.name === text));
-}
